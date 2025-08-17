96fe5e2d2ccb5e064fcf5d021e3679ae408acf06
Rajeev Tapadia <rtapadia730@gmail.com>
2025-08-17 21:23:24 +0530

    temp

--- main.cpp
+++ main.cpp
@@ -39,6 +39,110 @@ class GitRepo {
     }
 };
 
+class GitObject {
+    public:
+    std::string hash;
+    
+    GitObject(std::string content) {
+        this->hash = hash_object(content);
+    }
+    
+    // this class method will interact with fs to store any type of object in object store
+    void save_object() {}
+    
+    private:
+    std::string hash_object(std::string content) {
+        return "avc";
+    }
+};
+
+class GitBlob : public GitObject {
+public:
+    GitBlob(const std::string &filePath)
+        : GitObject(readFile(filePath))
+    {}
+
+private:
+    static std::string readFile(const std::string &filePath) {
+        std::ifstream file(filePath, std::ios::binary);
+        if (!file) {
+            throw std::runtime_error("Cannot open file: " + filePath);
+        }
+
+        std::ostringstream ss;
+        ss << file.rdbuf();
+        return ss.str();
+    }
+};
+
+class GitTree: public GitObject {
+    public:
+    // GitTree(fs::path dir_path): GitObject("asf") {
+    //     // construct a buffer which has the content of the tree and send to parent constructor
+    // }
+    
+    GitTree(std::string content): GitObject(content) {
+        save_object();
+    }
+};
+
+class Entry {
+public:
+    int perms;
+    std::string hash;
+    std::string type;
+    std::string name;
+
+    Entry(int perms, const std::string &hash, const std::string &type, const std::string &name) 
+        : perms(perms), hash(hash), type(type), name(name)
+    {}
+
+    std::string toString() const {
+        std::ostringstream ss;
+        ss << std::oct << perms << " ";   // permissions in octal
+        ss << type << " ";
+        ss << hash << "\t";
+        ss << name;
+        return ss.str();
+    }
+};
+
+Entry generate_tree_entry(const fs::directory_entry &child) {
+    int perms;
+    std::string type;
+    std::string hash;
+
+    if (child.is_regular_file()) {
+        perms = 0100644;      // typical blob file permissions
+        type = "blob";
+
+        // compute hash of file contents (placeholder here)
+        std::ifstream file(child.path(), std::ios::binary);
+        std::ostringstream ss;
+        ss << file.rdbuf();
+        std::string content = ss.str();
+
+        // For now, use content as "hash"; in real Git, you would SHA-1 it
+        hash = std::to_string(std::hash<std::string>{}(content));
+    } else if (child.is_directory()) {
+        perms = 040000;       // tree directory permissions in octal
+        type = "tree";
+
+        // For directories, compute tree hash recursively or placeholder
+        hash = "dummy_tree_hash";  // replace with actual tree hash logic
+    } else {
+        throw std::runtime_error("Unsupported file type: " + child.path().string());
+    }
+
+    std::string name = child.path().filename().string();
+    return Entry(perms, hash, type, name);
+}
+
+
+// class GitObjectStore {
+    
+// }
+
 std::string sha1_file(const std::string& filePath) {
     unsigned char hash[SHA_DIGEST_LENGTH];
     SHA_CTX shaCtx;
@@ -83,6 +187,7 @@ void save_blob(const std::string& file_path) {
 }
 
 // create a tree object
+
 // create recusively tree for each dir in repo
 // save tree object
 
